5ece211b2f239674b20e47aba835bb01bb1ed76f
Martin Liu <liuyinhsiang@Martins-MacBook-Pro.local>
2020-09-01 22:06:49 +1000

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { API } from 'aws-amplify';
+import { Link } from 'react-router-dom';
 import { useAppContext } from '../libs/contextLib';
 import { onError } from '../libs/errorLib';
 import './Home.css';
@@ -52,6 +53,14 @@ const Home = () => {
     <div className="lander">
       <h1>Scratch</h1>
       <p>A simple note taking app</p>
+      <div>
+        <Link to="/login" className="btn btn-info btn-lg">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
+      </div>
     </div>
   );
 
